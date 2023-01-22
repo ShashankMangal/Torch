9e950fbc237a50cae9f150882f9c0e29d8a4a6c9
Shashank Mangal <shashankmangal10@gmail.com>
2023-01-22 13:53:51 +0530

    app published on play store 2

--- app/src/main/java/com/sharkBytesLab/torch/MainActivity.java
+++ app/src/main/java/com/sharkBytesLab/torch/MainActivity.java
@@ -11,6 +11,8 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.applovin.sdk.AppLovinSdk;
+import com.applovin.sdk.AppLovinSdkConfiguration;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
@@ -30,6 +32,15 @@ public class MainActivity extends AppCompatActivity {
 
         torch = findViewById(R.id.torchButton);
         more = findViewById(R.id.more);
+
+        AppLovinSdk.getInstance( this ).setMediationProvider( "max" );
+        AppLovinSdk.initializeSdk( this, new AppLovinSdk.SdkInitializationListener() {
+            @Override
+            public void onSdkInitialized(final AppLovinSdkConfiguration configuration)
+            {
+                // AppLovin SDK is initialized, start loading ads
+            }
+        } );
 
         Dexter.withContext(this).withPermission(Manifest.permission.CAMERA).withListener(new PermissionListener() {
             @Override
