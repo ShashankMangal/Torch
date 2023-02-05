979b8aeded1e4aa69716658ed416d306c0063333
Shashank Mangal <shashankmangal10@gmail.com>
2023-02-05 10:18:48 +0530

    published version 5

--- app/src/main/java/com/sharkBytesLab/torch/MainActivity.java
+++ app/src/main/java/com/sharkBytesLab/torch/MainActivity.java
@@ -27,13 +27,10 @@ import com.karumi.dexter.listener.single.PermissionListener;
 
 import java.util.concurrent.TimeUnit;
 
-public class MainActivity extends AppCompatActivity implements MaxAdListener {
+public class MainActivity extends AppCompatActivity{
 
     private ImageButton torch, more;
     private boolean state;
-
-    private MaxInterstitialAd interstitialAd;
-    private int retryAttempt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,12 +48,6 @@ public class MainActivity extends AppCompatActivity implements MaxAdListener {
                 // AppLovin SDK is initialized, start loading ads
             }
         } );
-
-        interstitialAd = new MaxInterstitialAd( "19a73b4927442c34", this );
-        interstitialAd.setListener( this );
-
-        // Load the first ad
-        interstitialAd.loadAd();
 
         Dexter.withContext(this).withPermission(Manifest.permission.CAMERA).withListener(new PermissionListener() {
             @Override
@@ -85,11 +76,6 @@ public class MainActivity extends AppCompatActivity implements MaxAdListener {
         more.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                if ( interstitialAd.isReady() )
-                {
-                    interstitialAd.showAd();
-                }
                 startActivity(new Intent(MainActivity.this, MenuActivity.class));
 
             }
@@ -149,57 +135,6 @@ public class MainActivity extends AppCompatActivity implements MaxAdListener {
 
         });
 
-    }
-
-
-    // MAX Ad Listener
-    @Override
-    public void onAdLoaded(final MaxAd maxAd)
-    {
-        // Interstitial ad is ready to be shown. interstitialAd.isReady() will now return 'true'
-
-        // Reset retry attempt
-        retryAttempt = 0;
-        Toast.makeText(this, "Open Torch...", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onAdLoadFailed(final String adUnitId, final MaxError error)
-    {
-        // Interstitial ad failed to load
-        // AppLovin recommends that you retry with exponentially higher delays up to a maximum delay (in this case 64 seconds)
-
-        retryAttempt++;
-        long delayMillis = TimeUnit.SECONDS.toMillis( (long) Math.pow( 2, Math.min( 6, retryAttempt ) ) );
-
-        new Handler().postDelayed(new Runnable()
-        {
-            @Override
-            public void run()
-            {
-                interstitialAd.loadAd();
-            }
-        }, delayMillis );
-    }
-
-    @Override
-    public void onAdDisplayFailed(final MaxAd maxAd, final MaxError error)
-    {
-        // Interstitial ad failed to display. AppLovin recommends that you load the next ad.
-        interstitialAd.loadAd();
-    }
-
-    @Override
-    public void onAdDisplayed(final MaxAd maxAd) {}
-
-    @Override
-    public void onAdClicked(final MaxAd maxAd) {}
-
-    @Override
-    public void onAdHidden(final MaxAd maxAd)
-    {
-        // Interstitial ad is hidden. Pre-load the next ad
-        interstitialAd.loadAd();
     }
 
 }
--- app/src/main/java/com/sharkBytesLab/torch/MenuActivity.java
+++ app/src/main/java/com/sharkBytesLab/torch/MenuActivity.java
@@ -6,12 +6,22 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Toast;
 
-public class MenuActivity extends AppCompatActivity {
+import com.applovin.mediation.MaxAd;
+import com.applovin.mediation.MaxAdListener;
+import com.applovin.mediation.MaxError;
+import com.applovin.mediation.ads.MaxInterstitialAd;
 
-    private CardView ratings, review, terms, privacy;
+import java.util.concurrent.TimeUnit;
+
+public class MenuActivity extends AppCompatActivity implements MaxAdListener {
+
+    private CardView ratings, review, terms, privacy, support;
+    private MaxInterstitialAd interstitialAd;
+    private int retryAttempt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +32,13 @@ public class MenuActivity extends AppCompatActivity {
         review = findViewById(R.id.review);
         terms = findViewById(R.id.terms);
         privacy = findViewById(R.id.privacy);
+        support = findViewById(R.id.support_us);
 
+        interstitialAd = new MaxInterstitialAd( "19a73b4927442c34", this );
+        interstitialAd.setListener( this );
+
+        // Load the first ad
+        interstitialAd.loadAd();
 
         review.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,6 +54,16 @@ public class MenuActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "Error :" + e.getMessage(), Toast.LENGTH_SHORT).show();
                 }
 
+            }
+        });
+
+        support.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if ( interstitialAd.isReady() )
+                {
+                    interstitialAd.showAd();
+                }
             }
         });
 
@@ -92,4 +118,56 @@ public class MenuActivity extends AppCompatActivity {
         finish();
 
     }
+
+    // MAX Ad Listener
+    @Override
+    public void onAdLoaded(final MaxAd maxAd)
+    {
+        // Interstitial ad is ready to be shown. interstitialAd.isReady() will now return 'true'
+
+        // Reset retry attempt
+        retryAttempt = 0;
+        Toast.makeText(this, "Open Torch...", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onAdLoadFailed(final String adUnitId, final MaxError error)
+    {
+        // Interstitial ad failed to load
+        // AppLovin recommends that you retry with exponentially higher delays up to a maximum delay (in this case 64 seconds)
+
+        retryAttempt++;
+        long delayMillis = TimeUnit.SECONDS.toMillis( (long) Math.pow( 2, Math.min( 6, retryAttempt ) ) );
+
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                interstitialAd.loadAd();
+            }
+        }, delayMillis );
+    }
+
+    @Override
+    public void onAdDisplayFailed(final MaxAd maxAd, final MaxError error)
+    {
+        // Interstitial ad failed to display. AppLovin recommends that you load the next ad.
+        interstitialAd.loadAd();
+    }
+
+    @Override
+    public void onAdDisplayed(final MaxAd maxAd) {}
+
+    @Override
+    public void onAdClicked(final MaxAd maxAd) {}
+
+    @Override
+    public void onAdHidden(final MaxAd maxAd)
+    {
+        // Interstitial ad is hidden. Pre-load the next ad
+        interstitialAd.loadAd();
+    }
+
+
 }
